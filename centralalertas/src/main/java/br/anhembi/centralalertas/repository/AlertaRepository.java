5d42730bd09f4c2b7d9d8a89ae19d74c2bddf1c0
Caio Gonçalves <128558158+caiGon1@users.noreply.github.com>
2025-06-04 21:12:00 -0300

    Delete centralalertas directory

--- centralalertas/src/main/java/br/anhembi/centralalertas/repository/AlertaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.anhembi.centralalertas.repository;
-
-import br.anhembi.centralalertas.model.Alerta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AlertaRepository extends JpaRepository<Alerta, UUID> {
-}
