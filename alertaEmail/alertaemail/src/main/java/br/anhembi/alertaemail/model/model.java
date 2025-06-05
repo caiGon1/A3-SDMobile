f7e2cead5c7be9e8ce6e379722ae20f04a8ba6c4
Caio Gonçalves <128558158+caiGon1@users.noreply.github.com>
2025-06-04 21:14:54 -0300

    Add Email Conection

---
+++ alertaEmail/alertaemail/src/main/java/br/anhembi/alertaemail/model/model.java
@@ -0,0 +1,75 @@
+package br.anhembi.alertaemail.model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+
+@Entity
+@Table(name = "emails")
+public class model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long alertaId;
+    private Long userId;
+    private Long notifId;
+    private LocalDate data;
+    private String email;
+    private String destinatario;
+    private String assunto;
+    private String mensagem;
+    
+    public Long getAlertaId() {
+        return alertaId;
+    }
+    public void setAlertaId(Long alertaId) {
+        this.alertaId = alertaId;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getDestinatario() {
+        return destinatario;
+    }
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+    public String getAssunto() {
+        return assunto;
+    }
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+    public String getMensagem() {
+        return mensagem;
+    }
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    public Long getNotifId() {
+        return notifId;
+    }
+    public void setNotifId(Long notifId) {
+        this.notifId = notifId;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
