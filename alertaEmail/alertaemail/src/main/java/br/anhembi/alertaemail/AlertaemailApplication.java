7ebca21b42a777ff474bfa18f56ffd2a2469bd47
Caio Gonçalves <128558158+caiGon1@users.noreply.github.com>
2025-06-04 21:10:48 -0300

    Conexão com verificação

---
+++ alertaEmail/alertaemail/src/main/java/br/anhembi/alertaemail/AlertaemailApplication.java
@@ -0,0 +1,13 @@
+package br.anhembi.alertaemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaemailApplication.class, args);
+	}
+
+}
---
+++ alertaEmail/alertaemail/src/test/java/br/anhembi/alertaemail/AlertaemailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.anhembi.alertaemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
