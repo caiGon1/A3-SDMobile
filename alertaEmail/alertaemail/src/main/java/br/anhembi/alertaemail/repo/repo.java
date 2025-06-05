66171ecfcb1e3e5d032cf51c3280a9abb4e0a5bf
Caio Gonçalves <128558158+caiGon1@users.noreply.github.com>
2025-06-04 21:12:37 -0300

    Delete alertaEmail/alertaemail directory

--- alertaEmail/alertaemail/src/main/java/br/anhembi/alertaemail/repo/repo.java
+++
@@ -1,9 +0,0 @@
-package br.anhembi.alertaemail.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.anhembi.alertaemail.model.model;
-
-public interface repo extends JpaRepository<model, Long> {
-    boolean existsByNotifId(Long notifId);
-}
